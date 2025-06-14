b5a7fb0954fef1d2e1cc45793749d34aaf422177
jst-agu <webreg4site@gmail.com>
2025-06-14 14:39:17 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
   },
 };
 
